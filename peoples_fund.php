050989b74069b99df20255112a7cfbe3e4da4e5a
sunyata <sunyata@sunyata-P5QL-PRO>
2016-01-18 18:42:43 +0100

    First commit from NetBeans

---
+++ peoples_fund.php
@@ -0,0 +1,23 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * Plugin name: Peoples Fund
+ * Plugin URI: https://github.com/peoples-fund/peoples-fund
+ * Description: WordPress plugin for the People's Fund for Global Health and Development. Used for our own custom database and user payments
+ * Author: The People's Fund web programming team
+ * Version: 0.0.1
+ * Author URI: https://github.com/peoples-fund
+ * License: GPLv3
+ */
+
+// Paid memberships pro hook: When a donation is made: 1. intial 2. recurring
+// Action: Update database (db created if not already present? That's true for EA at least...)
+
+// 
+
